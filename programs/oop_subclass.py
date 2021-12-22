9c354c9357a6d2ec642e48e3afcac7d0b415b05a
Eugene Zuev <zhekazuev@gmail.com>
2021-12-22 05:04:53 +0100

    add russian docstrings, prints and comments

--- programs/oop_subclass.py
+++ programs/oop_subclass.py
@@ -1,45 +1,45 @@
 class SchoolMember:
-    '''Represents any school member.'''
+    '''Представляет любого человека в школе.'''
     def __init__(self, name, age):
         self.name = name
         self.age = age
-        print('(Initialized SchoolMember: {})'.format(self.name))
+        print('(Создан SchoolMember: {})'.format(self.name))
 
     def tell(self):
-        '''Tell my details.'''
-        print('Name:"{}" Age:"{}"'.format(self.name, self.age), end=" ")
+        '''Вывести информацию.'''
+        print('Имя:"{}" Возраст:"{}"'.format(self.name, self.age), end=" ")
 
 
 class Teacher(SchoolMember):
-    '''Represents a teacher.'''
+    '''Представляет преподавателя.'''
     def __init__(self, name, age, salary):
         SchoolMember.__init__(self, name, age)
         self.salary = salary
-        print('(Initialized Teacher: {})'.format(self.name))
+        print('(Создан Teacher: {})'.format(self.name))
 
     def tell(self):
         SchoolMember.tell(self)
-        print('Salary: "{:d}"'.format(self.salary))
+        print('Зарплата: "{:d}"'.format(self.salary))
 
 
 class Student(SchoolMember):
-    '''Represents a student.'''
+    '''Представляет студента.'''
     def __init__(self, name, age, marks):
         SchoolMember.__init__(self, name, age)
         self.marks = marks
-        print('(Initialized Student: {})'.format(self.name))
+        print('(Создан Student: {})'.format(self.name))
 
     def tell(self):
         SchoolMember.tell(self)
-        print('Marks: "{:d}"'.format(self.marks))
+        print('Оценки: "{:d}"'.format(self.marks))
 
 t = Teacher('Mrs. Shrividya', 40, 30000)
 s = Student('Swaroop', 25, 75)
 
-# prints a blank line
+# печатает пустую строку
 print()
 
 members = [t, s]
 for member in members:
-    # Works for both Teachers and Students
+    # работает как для преподавателя, так и для студента
     member.tell()
